a647380c5f4f3e83b4af2a3f19424b43b52acaa7
Steven Harrison <stevenmarkharrison@gmail.com>
2026-01-07 21:05:17 +0000

    test vm

--- lab/terraform/providers.tf
+++ lab/terraform/providers.tf
@@ -8,8 +8,9 @@ terraform {
 }
 
 provider "proxmox" {
-  pm_api_url      = "https://192.168.178.117:8006/api2/json"
-  pm_user         = "terraform@pve!terraform"
-  pm_api_token    = var.pm_api_token
-  pm_tls_insecure = true
+  endpoint = "https://192.168.178.117:8006"
+  insecure = true
+
+  # for API token auth:
+  api_token = var.pm_api_token
 }
