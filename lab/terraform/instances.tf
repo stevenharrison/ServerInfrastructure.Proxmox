4dacb16b42d0a5f466f8d18ed14fb29dae42f426
Steven Harrison <stevenmarkharrison@gmail.com>
2026-01-07 20:54:29 +0000

    test vm

---
+++ lab/terraform/instances.tf
@@ -0,0 +1,41 @@
+resource "proxmox_vm_qemu" "test_vm" {
+  name        = "tf-test-01"
+  target_node = "pve"
+  clone       = "ubuntu-template"
+
+  cores  = 1
+  memory = 1024
+
+  network {
+    bridge = "vmbr1"
+  }
+}
+
+resource "proxmox_vm_qemu" "test_vm" {
+  name        = "tf-iso-01"
+  target_node = "pve"
+
+  # Boot/install media already uploaded to Proxmox
+  iso = "local:iso/ubuntu-24.04.2-live-server-amd64.iso"
+
+  cores  = 1
+  memory = 1024
+
+  # VM disk (pick your storage)
+  scsihw = "virtio-scsi-pci"
+  disk {
+    slot    = "scsi0"
+    type    = "scsi"
+    storage = "local-lvm"
+    size    = "20G"
+  }
+
+  # NIC
+  network {
+    bridge = "vmbr1"
+    model  = "virtio"
+  }
+
+  # Boot order (syntax varies a bit by provider version)
+  boot = "order=ide2;scsi0;net0"
+}
